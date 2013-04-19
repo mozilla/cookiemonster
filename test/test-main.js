556c0e56b2dffcc96d8bb425b52f068073899540
Monica Chew <mmc@mozilla.com>
2013-04-19 15:15:43 -0700

    starting tests

--- test/test-main.js
+++ test/test-main.js
@@ -1,8 +1,18 @@
 var main = require("main");
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
-};
+const { Cc, Ci, Cu, Cr } = require("chrome");
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyServiceGetter(Services, "cookies",
+                                   "@mozilla.org/cookieService;1",
+                                   "nsICookieService");
+XPCOMUtils.defineLazyServiceGetter(Services, "cookiemgr",
+                                   "@mozilla.org/cookiemanager;1",
+                                   "nsICookieManager2");
+function testParseCookie() {
+  const kATTRIBUTES = { "expires": 0, "path": 0, "domain": 0, "max-age": 0 };
+}
 
 exports["test main async"] = function(assert, done) {
   assert.pass("async Unit test running!");
