b11d7c8dafa9a524a2ab8a58696cecd7f2cca926
David Dahl <david@ddahl.com>
2013-06-05 11:57:38 -0500

    added a few helpers, new analysis functions, running python analysis.py produces output for 3 histograms

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -6,24 +6,88 @@ import sys
 import numpy
 from pprint import pprint
 
-study_data_string = open("data.json")
+def get_cookie_data():
 
-print(study_data_string)
+    study_data_string = open("data.json")    
+    data = json.load(study_data_string)
+    cookie_data = []
 
-data = json.load(study_data_string)
+    for obj in data:
+        if obj["msg"]: 
+            if obj["msg"] == u"cookiemonster":
+                cookie_data.append(obj)
+    return cookie_data
 
-cookie_data = []
 
-for obj in study_data_string:
-    if obj.msg == "cookiemonster":
-        cookie_data.append(obj)
+def set_cookie_histogram(cookie_data):
 
-set_cookie_count = []
+    set_cookie_count = []
 
-for obj in cookie_data:
-    if obj.data.eventType == "set-cookie":    
-        set_cookie_count.append(obj.data.count)
+    for obj in cookie_data:
+        if obj["data"]["eventType"] == "set-cookie":
+            set_cookie_count.append(int(obj["data"]["count"]))
 
-h = numpy.histogram(cookie_data)
+    h = numpy.histogram(set_cookie_count)
+    return h
 
-pprint(h)
+# Do this with expiry times, domains via a map->numbers
+# 
+
+def make_domain_map(cookie_data):
+    """Map out the domains so we can reference them by number"""
+    domain_map = {}
+    domains = []
+    i = 0
+    for obj in cookie_data:
+        if obj["data"]:
+            try:
+                if obj["data"]["domain"] not in domains:
+                    d = obj["data"]["domain"]
+                    domain_map[i] = d
+                    domains.append(d)
+                    i = i + 1
+            except:
+                continue
+    return domain_map, domains
+
+
+def domain_histogram(domain_map):
+    d = []
+    for id in domain_map:
+        d.append(id)
+    
+    h = numpy.histogram(d)
+    return h
+
+
+def expiry_histogram(cookie_data):
+    maxage = []
+    for obj in cookie_data:
+        try:
+            if obj["data"]["maxage"]:
+                print(obj["data"]["maxage"])
+                maxage.append(int(obj["data"]["maxage"]))
+        except:
+            continue
+
+    h = numpy.histogram(maxage)
+    return h
+
+if __name__ == "__main__":
+    cd = get_cookie_data()
+    sch = set_cookie_histogram(cd)
+    dm, domains = make_domain_map(cd)
+    dh = domain_histogram(dm)
+    eh = expiry_histogram(cd)
+
+    print("set-cookie histogram:")
+    pprint(sch)
+    print("domain histogram:")
+    #pprint(dh)
+    _domains = []
+    for id in dh[0]:
+        _domains.append(dm[id])
+    dh3 = (_domains, dh[1],)
+    pprint(dh3)
+    print("expiry histogram:")
+    pprint(eh)
