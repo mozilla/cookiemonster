b0251ffebc864cf743f6af0b158061e9d5bac7a2
Monica Chew <mmc@mozilla.com>
2013-09-08 23:48:58 +0000

    Add per-user mapreduces

--- hadoop/user_cookie_mapper.py
+++ hadoop/user_cookie_mapper.py
@@ -10,28 +10,19 @@ try:
 except:
   import json
 
+# Usage: user_cookie_mapper <uid>
+# Expects a file named user_<uid>.history to exist with one domain per line
+
 # Each line is of the form
 # uid ts eventstoreid event-type event-subtype middle-final event-json
 # 1078540297	1370076131322	1	addon-main	--	m	{"msg": "addon-main", "eventstoreid": 1, "data": {"reason": "install", "version": "10.5.0"}, "ts": 1370076131322}
 
 NFIELDS = 7
 
-# Return a bucket for max age (in seconds)
-def bucket(max_age):
-  if max_age <= 60:
-    return "minute"
-  if max_age <= 60*60:
-    return "hour"
-  if max_age <= 24 * 60 * 60:
-    return "day"
-  if max_age <= 7 * 24 * 60 * 30:
-    return "week"
-  if max_age <= 30 * 24 * 60  * 60:
-    return "month"
-  if max_age <= 365 * 24 * 60  * 60:
-    return "year"
-  return "yearplus"
-
+f = open("user_%s.history" % sys.argv[1], "r")
+history = {}
+for l in f.readlines():
+  history[l.strip()] = 1
 
 for line in sys.stdin: 
   # Lines are tab-delimited
@@ -46,50 +37,47 @@ for line in sys.stdin:
   event_subtype = l[4]
   slice_type = l[5]
 
-  # Count the number of distinct users
-  print "UniqValueCount:users_total\t%s" % uid
+  if uid != sys.argv[1]:
+    continue
 
   # We only care about startup-shutdown and cookiemonster events for now
   if event_type != "cookiemonster":
     continue
-
-  # Count the number of cookie monster users
-  print "UniqValueCount:cm_users_total\t%s" % uid
-  print "LongValueSum:cm_user:%s\t1" % uid
 
   event = json.loads(l[-1])
   cm_event = event["data"]
   if cm_event["eventType"] != "set-cookie":
     continue
 
-  # Map to domains only
-  print "LongValueSum:%s:domain\t1" % cm_event["domain"]
+  # How long did they participate
+  print "LongValueMin:start_time\t%s" % ts
+  print "LongValueMax:end_time\t%s" % ts
 
-  # Ugh, we have some unparsable maxages.
-  if cm_event["maxage"]:
-    print "LongValueSum:maxage:%s\t1" % bucket(int(cm_event["maxage"]))
-    # Too many ages to be a histogram
-    age = int(cm_event["maxage"])
-    if age < 0:
-      age = 0
-    print "ValueHistogram:ages:%d\t1" % (age / 3600)
-
-  if cm_event["domain"] == cm_event["referrer"]:
+  # The domain of the cookie being set
+  domain = cm_event["domain"]
+  if domain == cm_event["referrer"]:
     # First party cookie
     print "LongValueSum:%s:first_party\t1" % cm_event["domain"]
     # Count up all first party cookies
-    print "LongValueSum:first_total\t1"
+    print "LongValueSum:first_party_total\t1"
   else:
     # Count up all first party cookies
-    print "LongValueSum:third_total\t1"
+    print "LongValueSum:third_party_total\t1"
     if int(cm_event["existingCount"]) > 0:
       # It would be accepted under FROM_VISITED
-      print "LongValueSum:%s:third_party_accepted\t1" % cm_event["domain"]
+      print "LongValueSum:%s:third_party_accepted\t1" % domain
       print "LongValueSum:third_party_accepted_total\t1"
     else:
       # It would be rejected under FROM_VISITED
-      print "LongValueSum:%s:third_party_rejected\t1" % cm_event["domain"]
+      print "LongValueSum:%s:third_party_rejected\t1" % domain
       print "LongValueSum:third_party_rejected_total\t1"
+    # Pretend they cleared their history
+    if domain in history:
+      print "LongValueSum:%s:third_party_history_accepted\t1" % domain
+      print "LongValueSum:third_party_history_accepted_total\t1"
+    else:
+      print "LongValueSum:%s:third_party_history_rejected\t1" % domain
+      print "LongValueSum:third_party_history_rejected_total\t1"
     # Print domain pairs
     print "LongValueSum:%s:%s:domain_pair\t1" % (
       cm_event["domain"], cm_event["referrer"])
